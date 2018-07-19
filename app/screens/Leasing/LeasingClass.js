234db5d452705299ba3e343fbb00ed6dc42940dc
Daniel Laurindo <dslaurindo@gmail.com>
2018-07-19 00:22:21 -0300

    Fix > husky

--- app/screens/Leasing/LeasingClass.js
+++ app/screens/Leasing/LeasingClass.js
@@ -85,9 +85,7 @@ export default class LeasingClass {
 
       const walletInfo = { seed: await StoreSeed.retrieveSeed() };
 
-      const {
-        toAddress, amount, fee, testnet
-      } = data;
+      const { toAddress, amount, fee, testnet } = data;
 
       const leaseData = {
         toAddress,
